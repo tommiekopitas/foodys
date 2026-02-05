ea77ca2bb4a71501e60af7e042c0ac367eb46909
Thomas Fouilloux <139991422+Thomas-F17@users.noreply.github.com>
2026-02-05 11:21:57 +0000

    Adding amended Q4(i)

--- Code/Q4(i).R
+++ Code/Q4(i).R
@@ -17,7 +17,7 @@ set.seed(1)
 N = 10000 # number of steps we run the MC for
 L = 1000 # last iterations plotted
 r = 10 # Number of cities
-beta_values = c(0.01,0.1,1) # chosen Betas = 0.01, 0.1, 1
+beta_values = seq(0, 20, by = 1)
 
 # initialising a matrix of zeros of size r+1 x r+1
 v <- matrix(0, nrow = r+1, ncol = r+1)
@@ -58,14 +58,15 @@ V <- function(x,v) {
   return (total)
 }
 
-# Set up plot area for our histograms
-par(mfrow = c(1, length(beta_values)))
+# Set up plot area for our averages
+par(mfrow = c(1, 1))
 
+# Setup the averages vector for plotting
+Averages <- c()
 
 # ==========
 # Step 2: Simulating the N iterations for each
-# beta in the beta_values vector and printing 
-# histograms
+# beta in the beta_values vector
 
 # Loop for beta values:
 for (betas in 1:length(beta_values)) {
@@ -88,13 +89,11 @@ for (n in 2:N){
   # The first thing we do in our loop is to generate a state from the 
   # proposal chain. Here, we set the proposal chain to be equal to our
   # current itinerary, and we pick two indices i and j uniformly at random
-  # from 1,...,r. We check that j is not equal to i (this happens about 10% of
-  # cases given we sample uniformly at random), if it's the case we generate
-  # more uniforms and accept the draw once j is different from i. Then, we swap
-  # values Y[i] and Y[j] in the proposal chain, hence, creating a new 
-  # permutation. Since we do not violate any properties of the permutation and
-  # we check that j is different from i, then y is always a valid permutation in
-  # our set C (of 10! possible permutations) and,thus, a feasible proposal.
+  # from 1,...,r (j and i will be equal about 10% of cases, we do not check for
+  # this). Then, we swap values Y[i] and Y[j] in the proposal chain, hence, 
+  # creating a new permutation. Since we do not violate any properties of the 
+  # permutation, then y is always a valid permutation in our set C (of 10!
+  # possible permutations) and, thus, a feasible proposal.
   
   Y <- X_current # We start by initialising Y as the current chain
   U1 = runif(1) # generate random uniform
@@ -102,14 +101,15 @@ for (n in 2:N){
   i = floor(U1*r)+1 # draw uniformly from {1,2,...,r}
   j = floor(U2*r)+1 # draw uniformly from {1,2,...,r}
 
-  # we ensure that j and i are always different
-  # by generating random uniforms until j is different from i
-  # i.e. we use rejection sampling to ensure that we select two 
-  # distinct indices
-  while (j == i) {
-    U2 = runif(1)
-    j = floor(U2*r)+1
-  }
+  # We do not ensure that j and i are always different:
+  # We could have done this by generating random uniforms until j is different 
+  # from i i.e. we use rejection sampling to ensure that we select two distinct 
+  # indices. We would have used the below loop:
+  # while (j == i) {
+  #   U2 = runif(1)
+  #   j = floor(U2*r)+1
+  # }
+  # In our case, repeating the permuations is deemed acceptable
   
   # we perform the swap to create a new and valid permutation
   temp <- Y[i]
@@ -145,18 +145,157 @@ for (n in 2:N){
   
 } # end of steps
 
-# We incorporate the histogram plotting within the beta loop so as to ensure
-# that one histogram is plotted for each value of beta
-# Note: we plot only the 1000 last values which is when our MC will have 
-# converged to the stationary distribution
-hist(X[(N-L+1):N], breaks = 50, xlab="states", ylab="Frequency", 
-     main=paste("Histogram of MC (β=",beta,")"))
+# Note: we only compute the averages for the 1000 last values which is when
+# our MC will have converged to the stationary distribution and it will also
+# have forgotten the initial state
 
-# We also compute the average of the distribution over the last L=1000 values
-print(paste("Average of the last", L, "values with beta", beta, "=", 
-            mean(X[(N-L+1):N])))
+# Print each beta and average to keep track of progress
+print(paste("Calculated for Beta =",beta, "with average", mean(X[(N-L+1):N])))
+# We add the maximum value found by the simulation
+print(paste("Max of", round(max(X),digits=2), "with Beta =", beta))
+
+# append the new computed average to the averages vector 
+Averages <- c(Averages,mean(X[(N-L+1):N]))
 
 } # end of loop for betas
+
+# ==========
+# Step 3: Choose Betas to simulate for N iterations
+
+# We reset seed to 1 given the previous use of random numbers
+set.seed(1)
+
+# We choose these three beta values given that they
+# represent the 3 transition states of the markov
+# decision process well:
+# - beta = 0.1 represents exploration
+# - beta = 3 represents balanced exploration and exploitation
+# - beta = 12 represents exploitation
+beta_values_chosen = c(0.1,3,12)
+beta_values_chosen_list <- list()
+
+# Note we reuse the same code as before, we simply 
+# add historgram plots after the beta loop
+
+# Loop for beta values:
+for (betas in 1:length(beta_values_chosen)) {
+  # we assign the beta value
+  beta <- beta_values_chosen[betas]
+  
+  # We start each iteration with an arbitrary 
+  # permutation 1,...,10
+  X_current <- c(1:r)
+  
+  # we store the value of this first permutation
+  X <- V(X_current,v)
+  
+  # we create a vector for each value of each state
+  X = c(X)
+  
+  # We now simulate all N iterations of our MC
+  for (n in 2:N){
+    # The first thing we do in our loop is to generate a state from the 
+    # proposal chain. Here, we set the proposal chain to be equal to our
+    # current itinerary, and we pick two indices i and j uniformly at random
+    # from 1,...,r (j and i will be equal about 10% of cases, we do not check 
+    # for this). Then, we swap values Y[i] and Y[j] in the proposal chain, hence
+    #, creating a new permutation. Since we do not violate any properties of the 
+    # permutation, then y is always a valid permutation in our set C (of 10! 
+    # possible permutations) and, thus, a feasible proposal.
+    
+    Y <- X_current # We start by initialising Y as the current chain
+    U1 = runif(1) # generate random uniform
+    U2 = runif(1) # generate random uniform
+    i = floor(U1*r)+1 # draw uniformly from {1,2,...,r}
+    j = floor(U2*r)+1 # draw uniformly from {1,2,...,r}
+    
+    # We do not ensure that j and i are always different:
+    # We could have done this by generating random uniforms until j is different 
+    # from i i.e. we use rejection sampling to ensure that we select two 
+    # distinct indices. We would have used the below loop:
+    # while (j == i) {
+    #   U2 = runif(1)
+    #   j = floor(U2*r)+1
+    # }
+    # In our case, repeating the permuations is deemed acceptable
+    
+    # we perform the swap to create a new and valid permutation
+    temp <- Y[i]
+    Y[i] <- Y[j]
+    Y[j] <- temp
+    
+    # As mentioned, we do not need to check for feasibility of Y as our choice
+    # is already a valid permutation in our set of permutations C.
+    
+    # We now calculate the value of the current itinerary 
+    # and the proposed itinerary
+    V_current = V(X_current,v)
+    V_prop = V(Y,v)
+    
+    # We calculate the acceptance probability
+    # Note: we use min between the exponential and 1 because if the proposal 
+    # chain has a higher value than the current (i.e. the exponential term is
+    # greater than 1), we accept it with probability 1.
+    exp_term = exp(beta*(V_prop - V_current))
+    prob_accept = min(exp_term,1) 
+    
+    # We generate the random uniform for the acceptance probability
+    U3 = runif(1)
+    
+    if (U3 < prob_accept){ # we accept
+      X_current = Y
+    }
+    # if we reject Y then X_current stays the same so we do nothing
+    
+    # We store the value of the current chosen itinerary in our X vector
+    # (which we later use to plot the stationary distribution).
+    X = c(X,V(X_current, v))
+    
+  } # end of steps
+  
+  # record the last 1000 values for each beta to plot the histograms
+  beta_values_chosen_list[[betas]] <- X[(N-L+1):N]
+  
+  # We also compute the average of the distribution over the last L=1000 values
+  print(paste("Average of the last", L, "values with beta", beta, "=", 
+              mean(X[(N-L+1):N])))
+  
+  # We add the maximum value found by the simulation
+  print(paste("Max of", round(max(X),digits=2), "with Beta =", beta))
+
+} # end of loop for betas
+
+
+# ==========
+# Step 4: Plotting the itinerary value vs beta graph and the histograms
+
+# setup the layout of the plots through a matrix
+layout_matrix <- matrix(c(1, 1, 1, 2, 3, 4), nrow = 2, byrow = TRUE)
+
+# set the matrix as the layout
+layout(layout_matrix)
+
+# Average Itinerary Value vs Beta
+# Plot the averages of the betas to exhibit the 3 transition states of the
+# markov decision process
+plot(beta_values, Averages, type = "l", 
+     xlab = "Beta", ylab = "Average Value (last 1000 iterations)",
+     main = "Average Itinerary Value vs Beta",
+     xaxt = "n")
+axis(1, at = 1:20)
+abline(v = 1:20, col = "lightgray", lty = "dotted")
+abline(h = axTicks(2), col = "lightgray", lty = "dotted")
+
+# We incorporate the histogram plotting outside of the beta loop so as to ensure
+# that all plots can be included in the output of the code
+# Note: as mentioned, we plot only the 1000 last values which is when our MC
+# will have converged to the stationary distribution
+for (betas in 1:length(beta_values_chosen)) {
+  hist(beta_values_chosen_list[[betas]], breaks = 50, xlab="states", ylab=
+         "Frequency", main=paste("Histogram of MC (Beta =",
+          beta_values_chosen[betas],"\n and mean =",
+          round(mean(beta_values_chosen_list[[betas]]),2),")"))
+}
 
 # reset the plot area to avoid errors
 par(mfrow = c(1, 1))
